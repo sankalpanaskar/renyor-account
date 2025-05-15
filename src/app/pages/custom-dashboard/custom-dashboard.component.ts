efa3b5feba9feb59b125688e19d20ac4638147f5
Misbah Chowdhury <misbah.chowadhury@anudip.org>
2025-05-15 16:05:43 +0530

    changes

--- src/app/pages/custom-dashboard/custom-dashboard.component.ts
+++ src/app/pages/custom-dashboard/custom-dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./custom-dashboard.component.scss']
 })
 export class CustomDashboardComponent {
-
+//M
   // Summary Cards
   stats = [
     { title: "Today's Leads", value: 45 },
