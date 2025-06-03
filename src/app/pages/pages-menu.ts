87ad564f0fce875daa49d3749ca66607cc914c5e
Misbah Chowdhury <misbah.chowadhury@anudip.org>
2025-06-03 14:04:56 +0530

    new last updated latest

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -13,7 +13,7 @@ export function getMenuItems(roleId: any): NbMenuItem[] {
     //   icon: 'home-outline',
     //   link: '/pages/iot-dashboard',
     // },
-    ...(roleId === 34 || roleId === 11 || roleId === 15 || roleId === 17 || roleId === 18 || roleId === 43 || roleId === 45 || roleId === 44 || roleId === 46 || roleId === 10 || roleId === 26 || roleId === 37 || roleId === 42
+    ...(roleId === 34 || roleId === 11 || roleId === 15 || roleId === 17 || roleId === 18 || roleId === 43 || roleId === 45 || roleId === 44 || roleId === 46 || roleId === 10 || roleId === 26 || roleId === 37 || roleId === 42 || roleId === 7 || roleId === 23 || roleId === 23 || roleId === 44
       ? [
         {
           title: 'Follow-up Dashboard',
