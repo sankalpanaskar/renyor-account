7241ca85bf00232ef4acc1be92f0e37cebccb7b5
sankalpa1992 <sankalpa.naskar@anudip.org>
2025-12-12 01:44:40 +0530

    updated at 12/12/2025

---
+++ src/app/pages/settings/settings.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
+import { NbThemeService, NbToastrService } from '@nebular/theme';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'ngx-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
+})
+export class SettingsComponent implements OnInit {
+
+  model: any = [];
+  isSubmitting = false;
+
+  constructor(
+    public globalService: GlobalService,
+    private toastrService: NbToastrService,
+    private themeService: NbThemeService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+   
+  }
+
+}
+
