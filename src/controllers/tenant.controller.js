d1d05f31f914809ca73b863630895427d7a40bb6
anudiparup <arup.das@anudip.org>
2026-01-19 20:11:40 +0530

    first

--- src/controllers/tenant.controller.js
+++ src/controllers/tenant.controller.js
@@ -2,7 +2,11 @@ const TenantService = require('../services/tenant.service');
 
 exports.create = async (req, res) => {
   try {
-    return 0;
+    return res.success(
+      200,
+      "Tenant created successfully",
+      1
+    );
     const tenant = await TenantService.create(req.body);
     
     //return 0;
