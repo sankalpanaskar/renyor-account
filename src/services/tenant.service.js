aa5d9b25bd8b8b45cc41f553a63821c4603a69d2
anudiparup <arup.das@anudip.org>
2026-01-19 21:26:43 +0530

    first

--- src/services/tenant.service.js
+++ src/services/tenant.service.js
@@ -88,6 +88,6 @@ exports.create = async (data) => {
 
 
 exports.getAll = async () => {
-  const result = await db.query('SELECT * FROM tenants ORDER BY id');
-  return result.rows;
+  const [result] = await db.query('SELECT * FROM tenants ORDER BY id');
+  return result;
 };
