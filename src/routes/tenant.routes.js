285e3103c68fa4b8ad252bfae48a70468798d684
anudiparup <arup.das@anudip.org>
2026-01-19 20:06:30 +0530

    first

--- src/routes/tenant.routes.js
+++ src/routes/tenant.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const TenantController = require('../controllers/tenant.controller');
 const authSuperadmin = require('../middleware/authSuperAdmin');
 
-router.post('/create-tenant', authSuperadmin, TenantController.create);
+router.post('/create-tenant', TenantController.create);
 router.get('/get-tenant', authSuperadmin, TenantController.getAll);
 
 module.exports = router;
