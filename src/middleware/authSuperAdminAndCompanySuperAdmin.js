b87ef54d6d5e709f97d6c0b6b12c36f03948333f
anudiparup <arup.das@anudip.org>
2026-03-01 01:06:29 +0530

    menu create

---
+++ src/middleware/authSuperAdminAndCompanySuperAdmin.js
@@ -0,0 +1,47 @@
+const { verifyToken } = require('../utils/jwt');
+const db = require('../config/db');
+
+module.exports = async function authSuperadmin(req, res, next) {
+  const header = req.headers.authorization || '';
+  const token = header.startsWith('Bearer ') ? header.slice(7) : null;
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  try {
+    // Verify JWT
+    const payload = verifyToken(token);
+    
+
+    // MySQL query (use ?)
+    const [rows] = await db.query(
+      `SELECT id, tenant_id, role_id, email, name, is_system_super_admin,is_company_super_admin
+       FROM users WHERE id = ?`,
+      [payload.userId]
+    );
+   
+    if (rows.length === 0) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+    console.log(rows[0].is_system_super_admin,rows[0].is_company_super_admin);
+    // MySQL boolean check (1 = true)
+    if (
+        rows[0].is_system_super_admin !== 1 &&
+        rows[0].is_company_super_admin !== 1
+      ) {
+        console.log('ss');
+        return res.status(403).json({
+          error: 'Only Project Super Admin and Company Superadmin can do this'
+        });
+      }
+
+    // Attach user to request
+    req.user = rows[0];
+    next();
+
+  } catch (err) {
+    console.error('authSuperadmin error:', err.message);
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
