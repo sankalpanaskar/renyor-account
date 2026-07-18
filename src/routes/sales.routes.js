609794f0f641fc571326fb5c6f05df2c8b615a0d
anudiparup <arup.das@anudip.org>
2026-07-18 20:33:24 +0530

    invoice slove

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -84,7 +84,7 @@ router.get('/fetch-units', auth, salesController.fetchUnits);
 router.post('/insert-payment-term', auth, formDataUpload.none(), salesController.insertPaymentTerm);
 router.get('/fetch-payment-terms', auth, salesController.fetchPaymentTerms);
 router.post('/create-tax-rate', auth, formDataUpload.none(), salesController.createTaxRate);
-router.post('/create-invoice', auth,formDataUpload.none().salesController.createInvoice);
+router.post('/create-invoice', auth,formDataUpload.none(),salesController.createInvoice);
 router.post('/generate-pdf', auth, formDataUpload.none(), salesController.createDocumentPdf);
 router.get('/demo-invoice-pdf', auth, salesController.createDemoInvoicePdf);
 router.get('/demo-quotation-pdf', auth, salesController.createDemoQuotationPdf);
